1f3b142095c2310e5058104c6c4f8f2e91be6160
mah00j <mah06.hajj@gmail.com>
2025-12-03 18:45:33 +0200

    feat: Update login API endpoint to production URL

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -16,7 +16,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/login",
+        "https://hunitalk-production.up.railway.app/api/login",
         {
           email,
           password,
